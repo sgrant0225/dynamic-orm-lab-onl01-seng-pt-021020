e863a93535af77130df2aebfdec8b7e2aa9e2baf
Sharnell Grant <sharnellgrant@gmail.com>
2020-04-19 00:26:36 +0000

    Done.

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -1,6 +1,23 @@
 require_relative "../config/environment.rb"
 require 'active_support/inflector'
-
+require 'pry'
 class InteractiveRecord
   
+  def self.column_names
+   DB[:conn].results_as_hash = true
+   sql = "PRAGMA table_info('#{table_name}')"
+   table_info = DB[:conn].execute(sql)
+   column_names = []
+   table_info.each do |row|
+     column_names << row["name"]
+   end
+   column_names.compact
+ end
+ 
+ def initialize(options={})
+   options.each do |property, value|
+     self.send("#{property}=", value)
+   end
+  end 
+   
 end
